46ed75ff6b82f29af630ef4538e140ae916442a4
吴志易 <wuzhiyi@wuzhiyideMacBook-Air.local>
2015-09-23 13:35:08 +0800

    test pull request

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;		// The position that that camera will be following.
+	public float smoothing = 5f;	// The speed with which the camera will be following.
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		// Calculate the initial offset.
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Create a position the camera is aiming for based on the offset from the target.
+		Vector3 targetCamPos = target.position + offset;
+
+		// Smoothly interpolate between the camera's current position and it's target position.
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
